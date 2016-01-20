03fd1fdcc049619365bd2ca827a1dd90806cc6f0
Oharoogu <chrhwkdrn@naver.com>
2016-01-20 15:46:13 +0900

    ContentAction.java apply

--- src/com/board/action/ContentAction.java
+++ src/com/board/action/ContentAction.java
@@ -1,5 +1,19 @@
 package com.board.action;
 
-public class ContentAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.board.beans.Board;
+import com.board.controller.CommandAction;
+import com.board.dao.BoardDao;
 
+public class ContentAction implements CommandAction {
+
+	@Override
+	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
+		int idx = Integer.parseInt(request.getParameter("idx"));
+		
+		Board article = BoardDao.getInstance().getArticle(idx);
+		request.setAttribute("article", article);
+		return "content.jsp";
+	}
 }
