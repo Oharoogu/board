13863f8e17434911c0a917e0e5b33ec0881fc6bc
Oharoogu <chrhwkdrn@naver.com>
2016-01-21 16:01:33 +0900

    DeleteAction.java 게시글 삭제시 파일도 같이 삭제하기 추가

--- src/com/board/action/DeleteAction.java
+++ src/com/board/action/DeleteAction.java
@@ -2,6 +2,8 @@ package com.board.action;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import com.board.beans.Board;
 import com.board.controller.CommandAction;
 import com.board.dao.BoardDao;
 
@@ -9,6 +11,16 @@ public class DeleteAction implements CommandAction{
 	@Override
 	public String requestPro(HttpServletRequest request, HttpServletResponse response) throws Throwable {
 		int idx = Integer.parseInt(request.getParameter("idx"));
+		
+		Board article = BoardDao.getInstance().getArticle(idx);
+		String filename = article.getFilename();
+		String uploadFileName = request.getRealPath("/upload") +"/"+ filename;
+
+		File uploadfile = new File (uploadFileName);
+		if ( uploadfile.exists()&& uploadfile.isFile() )
+		{
+			 uploadfile.delete();		// 파일 삭제
+		}
 		
 		BoardDao.getInstance().deleteArticle(idx);
 		return "delete.jsp";
